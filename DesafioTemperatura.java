95f8ed3e5fcbabde2a2418ee0795623e01b3de8d
Pablo Ramirez Cabrera <pablo.ramirez9917@gmail.com>
2024-12-30 01:50:04 -0600

    DesafioTemperatura

---
+++ DesafioTemperatura.java
@@ -0,0 +1,8 @@
+public class DesafioTemperatura{
+    public static void main(String[] args){
+        double Temperatura = 26.5;
+        double Conversion = (Temperatura * 1.8) + 32;
+        
+        System.out.println("La temperatura en celcius es de "+Temperatura+" en Fahrenheit es de "+Conversion);
+    }
+}
